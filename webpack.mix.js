7a0afe8ca2150a1a4a9313444f9078d74d94ed21
Marco Rieser <marco.rieser@jedesign.ch>
2021-10-03 14:27:19 +0200

    install inertia vue3 components

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix
+    .js('resources/js/app.js', 'public/js')
+    .vue(3)
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
+    .version()
+;
